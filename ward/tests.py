0f2054fffdb6c6e60f13872f8cc252aa749b1fa8
Tony Kihara <tonykiharatonkin@gmail.com>
2019-08-04 18:25:59 +0300

    added the tests.py file.

---
+++ ward/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from .models import User, Profile , Image
+from django.contrib.auth.models import User
+
+# Create your tests here.
+class UserTestClass(TestCase):
+    def setUp(self):
+        self.
